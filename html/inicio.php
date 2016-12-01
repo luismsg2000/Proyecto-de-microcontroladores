ee39f1079ee18e42bd6602432e9925f065121f73
Luis Sanchez <luismsg2000@gmail.com>
2016-12-01 00:09:15 -0600

    Se crea pagina web de inicio de sesion

---
+++ html/inicio.php
@@ -0,0 +1,40 @@
+<html>
+
+<head>
+
+<title></title>
+
+</head>
+
+<body background="parqueo1.jpg">
+
+<p>Ingreso de nuevos usuarios</p>
+
+<br />
+
+<?PHP
+
+?>
+
+</body>
+
+
+
+<form action="ingresar.php" method="post">
+ <p> Escriba la placa: <input type="text" name="placa" /></p> 
+ <p><input type="submit" value="Ingresar"/></p>
+ 
+</form>
+
+
+
+<form action="cobros.php" method="post">
+ <p><input type="submit" value="Ir a cobros"/></p>
+ 
+</form>
+
+
+
+
+
+</html>
